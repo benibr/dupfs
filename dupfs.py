d7ef4c98788954f133a414771a24e2bed2736338
Beni <braunger@zib.de>
2025-01-08 20:59:48 +0100

    prepared 2nd root

--- dupfs.py
+++ dupfs.py
@@ -9,43 +9,51 @@ import errno
 from fuse import FUSE, FuseOSError, Operations, fuse_get_context
 
 
-class Passthrough(Operations):
-    def __init__(self, root):
-        self.root = root
+class dupfs(Operations):
+    def __init__(self, root1, root2):
+        self.root1 = root1
+        self.root2 = root2
 
     # Helpers
     # =======
 
-    def _full_path(self, partial):
+    def _full_path_root1(self, partial):
         if partial.startswith("/"):
             partial = partial[1:]
-        path = os.path.join(self.root, partial)
+        path = os.path.join(self.root1, partial)
+        return path
+
+    def _full_path_root2(self, partial):
+        if partial.startswith("/"):
+            partial = partial[1:]
+        path = os.path.join(self.root2, partial)
         return path
 
     # Filesystem methods
     # ==================
 
     def access(self, path, mode):
-        full_path = self._full_path(path)
+        full_path = self._full_path_root2(path)
         if not os.access(full_path, mode):
             raise FuseOSError(errno.EACCES)
 
     def chmod(self, path, mode):
-        full_path = self._full_path(path)
+        full_path = self._full_path_root2(path)
         return os.chmod(full_path, mode)
 
-    def chown(self, path, uid, gid):
-        full_path = self._full_path(path)
+    def chown(self, path, path2, uid, gid):
+        full_path = self._full_path_root2(path)
+        os.chown(full_path, uid, gid)
         return os.chown(full_path, uid, gid)
 
     def getattr(self, path, fh=None):
-        full_path = self._full_path(path)
+        full_path = self._full_path_root2(path)
         st = os.lstat(full_path)
         return dict((key, getattr(st, key)) for key in ('st_atime', 'st_ctime',
                      'st_gid', 'st_mode', 'st_mtime', 'st_nlink', 'st_size', 'st_uid'))
 
     def readdir(self, path, fh):
-        full_path = self._full_path(path)
+        full_path = self._full_path_root2(path)
 
         dirents = ['.', '..']
         if os.path.isdir(full_path):
@@ -54,7 +62,7 @@ class Passthrough(Operations):
             yield r
 
     def readlink(self, path):
-        pathname = os.readlink(self._full_path(path))
+        pathname = os.readlink(self._full_path_root2(path))
         if pathname.startswith("/"):
             # Path name is absolute, sanitize it.
             return os.path.relpath(pathname, self.root)
@@ -62,47 +70,47 @@ class Passthrough(Operations):
             return pathname
 
     def mknod(self, path, mode, dev):
-        return os.mknod(self._full_path(path), mode, dev)
+        return os.mknod(self._full_path_root2(path), mode, dev)
 
     def rmdir(self, path):
-        full_path = self._full_path(path)
+        full_path = self._full_path_root2(path)
         return os.rmdir(full_path)
 
     def mkdir(self, path, mode):
-        return os.mkdir(self._full_path(path), mode)
+        return os.mkdir(self._full_path_root2(path), mode)
 
     def statfs(self, path):
-        full_path = self._full_path(path)
+        full_path = self._full_path_root2(path)
         stv = os.statvfs(full_path)
         return dict((key, getattr(stv, key)) for key in ('f_bavail', 'f_bfree',
             'f_blocks', 'f_bsize', 'f_favail', 'f_ffree', 'f_files', 'f_flag',
             'f_frsize', 'f_namemax'))
 
     def unlink(self, path):
-        return os.unlink(self._full_path(path))
+        return os.unlink(self._full_path_root2(path))
 
     def symlink(self, name, target):
-        return os.symlink(target, self._full_path(name))
+        return os.symlink(target, self._full_path_root2(name))
 
     def rename(self, old, new):
-        return os.rename(self._full_path(old), self._full_path(new))
+        return os.rename(self._full_path_root2(old), self._full_path_root2(new))
 
     def link(self, target, name):
-        return os.link(self._full_path(name), self._full_path(target))
+        return os.link(self._full_path_root2(name), self._full_path_root2(target))
 
     def utimens(self, path, times=None):
-        return os.utime(self._full_path(path), times)
+        return os.utime(self._full_path_root2(path), times)
 
     # File methods
     # ============
 
     def open(self, path, flags):
-        full_path = self._full_path(path)
+        full_path = self._full_path_root2(path)
         return os.open(full_path, flags)
 
     def create(self, path, mode, fi=None):
         uid, gid, pid = fuse_get_context()
-        full_path = self._full_path(path)
+        full_path = self._full_path_root2(path)
         fd = os.open(full_path, os.O_WRONLY | os.O_CREAT, mode)
         os.chown(full_path,uid,gid) #chown to context uid & gid
         return fd
@@ -116,7 +124,7 @@ class Passthrough(Operations):
         return os.write(fh, buf)
 
     def truncate(self, path, length, fh=None):
-        full_path = self._full_path(path)
+        full_path = self._full_path_root2(path)
         with open(full_path, 'r+') as f:
             f.truncate(length)
 
@@ -130,9 +138,9 @@ class Passthrough(Operations):
         return self.flush(path, fh)
 
 
-def main(mountpoint, root):
-    FUSE(Passthrough(root), mountpoint, nothreads=True, foreground=True, allow_other=True)
+def main(root1, root2, mountpoint):
+    FUSE(dupfs(root1, root2), mountpoint, nothreads=True, foreground=True, allow_other=True)
 
 
 if __name__ == '__main__':
-    main(sys.argv[2], sys.argv[1])
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
